f9183501c188863b5f94199f3c183d2edd1d46d2
Sara Öhman <saraohman97@gmail.com>
2024-03-05 13:04:42 +0100

    Stripe done.

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,7 +4,6 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-
 export const formatter = new Intl.NumberFormat("en-US", {
   style: "currency",
   currency: "USD"
